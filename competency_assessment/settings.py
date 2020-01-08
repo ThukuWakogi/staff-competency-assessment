2773d79c2ca1a664c44734cb637515d18ac46e4e
Brian Otieno <bryomajor@gmail.com>
2020-01-08 12:55:46 +0300

    register the database and change the timezone.

--- competency_assessment/settings.py
+++ competency_assessment/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'competency_assessment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'staff_assessment',
+        'USER': 'moringa',
+        'PASSWORD': 'p@$$w0rd',
     }
 }
 
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
