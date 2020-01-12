65496218da34ca6cafb8b9d553e56ef471f75e37
Brian Otieno <bryomajor@gmail.com>
2020-01-12 16:58:01 +0300

    set up a custom user auth.

--- competency_assessment/assessment/admin.py
+++ competency_assessment/assessment/admin.py
@@ -1,42 +1,29 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
-# Register your models here.
-class CustomUserAdmin(BaseUserAdmin):
-    # The forms to add and change user instances
-    form = CustomUserChangeForm
-    add_form = CustomUserCreationForm
 
-    # The fields to be used in displaying the User model.
-    # These override the definitions on the base UserAdmin
-    # that reference specific fields on auth.User.
-    list_display = ('email', 'is_admin')
-    list_filter = ('is_admin',)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        # ('Level info', {'fields': ('level',)}),
-        ('Permissions', {'fields': ('is_admin',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
-    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
-    # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
-        }),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
     )
     search_fields = ('email',)
     ordering = ('email',)
-    filter_horizontal = ()
 
 
-# Now register the new UserAdmin...
 admin.site.register(CustomUser, CustomUserAdmin)
-# ... and, since we're not using Django's built-in permissions,
-# unregister the Group model from admin.
-# admin.site.unregister(Group)
 admin.site.site_header = "HR Dashboard"
