4d1b0a222ca826c640d33206ca3ee3949c3a87eb
Brian Otieno <bryomajor@gmail.com>
2020-01-12 16:59:33 +0300

    test if user class is instantiated correctly and saved.

--- competency_assessment/urls.py
+++ competency_assessment/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
