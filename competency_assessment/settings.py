12b5aa69d0823ff769d4b39d355d5e265ace0293
Steve-design <smzalendo31@gmail.com>
2020-01-14 10:36:20 +0300

    update database

--- competency_assessment/settings.py
+++ competency_assessment/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'assessment',
-        'USER': 'n',
-        'PASSWORD': 'p@$$w0rd',
+        'USER': 'nancy',
+        'PASSWORD': 'nkm',
     }
 }
 
