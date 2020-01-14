33b5a2bd1a78495070ea3e12624d4057ef45be5d
Steve-design <smzalendo31@gmail.com>
2020-01-14 09:56:55 +0300

    import assessment period and register in the admin site

--- competency_assessment/assessment/admin.py
+++ competency_assessment/assessment/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Assessment_period
 
 
 admin.site.register(User)
+admin.site.register(Assessment_period)
 admin.site.site_header = "HR Dashboard"
