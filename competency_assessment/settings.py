fa5496c78a983abff06c9d704419afc67fa692b3
Steve-design <smzalendo31@gmail.com>
2020-01-14 11:35:42 +0300

    modify settings.py

--- competency_assessment/settings.py
+++ competency_assessment/settings.py
@@ -79,15 +79,9 @@ WSGI_APPLICATION = 'competency_assessment.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-<<<<<<< HEAD
         'NAME': os.environ['DB_NAME'] if 'DB_NAME' in os.environ else None,
         'USER': os.environ['DB_USER'] if 'DB_USER' in os.environ else None,
         'PASSWORD': os.environ['DB_PASSWORD'] if 'DB_PASSWORD' in os.environ else None,
-=======
-        'NAME': 'assessment',
-        'USER': 'nancy',
-        'PASSWORD': 'nkm',
->>>>>>> feature-assessments
     }
 }
 
