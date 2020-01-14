3656571df1d3596260e4e92a237166bb8a6a395d
Brian Otieno <bryomajor@gmail.com>
2020-01-14 11:06:46 +0300

    Extend the base class that Django has for User models such that we exclude username and also making the email field required.

--- competency_assessment/assessment/models.py
+++ competency_assessment/assessment/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, User as AuthUser
+from django.contrib.auth.models import AbstractUser, User as AuthUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 
@@ -15,6 +15,12 @@ class User(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     level = models.ForeignKey('Level', blank=True, null=True, on_delete=models.CASCADE)
     date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
 
     def __str__(self):
         return self.email
