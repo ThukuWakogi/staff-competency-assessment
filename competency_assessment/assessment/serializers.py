58abad15495b66a2e6dfa4f26bdd793e1b0dce96
Steve-design <smzalendo31@gmail.com>
2020-01-15 10:32:14 +0300

    create the assessment result serializer

--- competency_assessment/assessment/serializers.py
+++ competency_assessment/assessment/serializers.py
@@ -113,3 +113,12 @@ class AssessmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Assessment 
         fields = ('user_id','assessment_period', 'is_assessed_by_manager', 'is_assessed_after_norming')
+
+class ResultsSerializer(serializers.ModelSerializer):
+    user_id = UserSerializer(many=True, read_only=True)
+    assessment = AssessmentSerializer(many=True, read_only=True)
+    strand = StrandSerializer(many=True,read_only=True)
+    rating = RatingSerializer(many=True,read_only=True)
+    class Meta:
+        model = Assessment_results
+        fields = ('user_id', 'assessment', 'competency', 'strand', 'rating')
