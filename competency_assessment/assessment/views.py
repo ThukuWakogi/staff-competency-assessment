381e6ac66322234335e260dd8ee3d2f1a3dd288b
Brian Otieno <bryomajor@gmail.com>
2020-01-15 10:33:17 +0300

    import generics module.

--- competency_assessment/assessment/views.py
+++ competency_assessment/assessment/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
