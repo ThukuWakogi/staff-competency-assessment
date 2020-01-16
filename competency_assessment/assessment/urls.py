f2d07db1118e9047a4a13ad498a50ae456a72225
Steve-design <smzalendo31@gmail.com>
2020-01-16 09:20:44 +0300

    create an end point for the strands

--- competency_assessment/assessment/urls.py
+++ competency_assessment/assessment/urls.py
@@ -9,6 +9,7 @@ router.register(r'assessments', views.AssessmentViewSet),
 router.register(r'ratings', views.RatingViewSet),
 router.register(r'assessment-results', views.AssessmentResultViewSet),
 router.register(r'competencies',views.CompetencyViewSet),
+router.register(r'strands', views.StrandViewSet),
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', views.ObtainAuthTokenAndUserDetails.as_view()),
